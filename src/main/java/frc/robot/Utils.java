8c7f0a8041c3abe41bbd50722f4ef450975d750e
James-Caruso <james.caruso.2025@gmail.com>
2025-11-04 13:27:21 -0500

    Update Utils.java

--- src/main/java/frc/robot/Utils.java
+++ src/main/java/frc/robot/Utils.java
@@ -1,5 +1,4 @@
 package frc.robot;
-
 public class Utils {
     public static boolean IsDoubleApproximately(double left, double right, double sigma){
         return left >= right - sigma && left <= right + sigma;
