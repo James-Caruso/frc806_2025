7d435fc7f1c9444ae2e20c770a958afd2cfb10e5
James-Caruso <james.caruso.2025@gmail.com>
2025-11-04 13:22:53 -0500

    Update DriveFieldRelative.java

--- src/main/java/frc/robot/Commands/DriveFieldRelative.java
+++ src/main/java/frc/robot/Commands/DriveFieldRelative.java
@@ -1,11 +1,9 @@
 package frc.robot.Commands;
-
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 import frc.robot.Subsystems.DrivetrainSubsystem;
-
 public class DriveFieldRelative extends Command {
   private final DrivetrainSubsystem swerve;
   private final CommandXboxController controller;
@@ -44,7 +42,6 @@ public class DriveFieldRelative extends Command {
     theta = (theta > 0) ? Math.abs(Math.pow(theta, rotationPow)) : -Math.abs(Math.pow(theta, rotationPow));
     double slowModeFactor = (controller.getLeftTriggerAxis() * Constants.Drivetrain.SlowFactor) + Constants.Drivetrain.SlowFactorOffset;
     boolean isCosineCompensated = controller.getRightTriggerAxis() >= 0.5;
-
     swerve.driveFieldRelative(
       new ChassisSpeeds(
         (y * Constants.attainableMaxTranslationalSpeedMPS) / slowModeFactor, 
