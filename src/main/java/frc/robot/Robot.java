acf339c23a10b349205c7b9bbea1e810f6638228
James-Caruso <james.caruso.2025@gmail.com>
2025-12-03 22:56:24 +0000

    cleaning up

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,16 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-package frc.robot;
+// the WPILib BSD license file in the root directory of this project.package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Commands.DriveFieldRelative;
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-  private RobotContainer m_robotContainer;     
-  @Override
-  public void robotInit() {
-    enableLiveWindowInTest(true);
+    private Command m_autonomousCommand;
+    private RobotContainer m_robotContainer;     
+    @Override
+    public void robotInit() {
+        enableLiveWindowInTest(true);
     m_robotContainer = new RobotContainer();
     //CameraServer.startAutomaticCapture();
     //processor = new Processor();
